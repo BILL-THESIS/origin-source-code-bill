f95ab3b2806c23f60d97cd0a34edda9a5f648fdb
beaw21 <beawglom2013@gmail.com>
2023-07-02 20:38:17 +0700

    Data set to prepare separate groups of labels

---
+++ models/01.seatunnel_extract.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from datetime import timedelta
+
+git_df = pd.read_csv("../Github/output/02.seatunnel_check_pull.csv")
+sonar_project_key = pd.read_csv("../Sonar/output/all_projects_data.csv")
+sonar_smells = pd.read_csv("../Sonar/output/smells_all_seatunnel.csv")
+
+sonar_smells.set_index(['key'])
+
+git_ture = git_df.drop(git_df[git_df['merged'] == False].index)
+
+df_extract = []
+sonar_key_set = []
+
+sonar_key_set = pd.DataFrame({
+                                'key': sonar_project_key['key'],
+                                'revision': sonar_project_key['revision']
+                            })
+sonar_project_key.set_index(['revision'])
+
+df_extract_begin = pd.DataFrame({
+                                    'sha': git_ture['base.sha'],
+                                    'time': git_ture['created_at']
+                                })
+df_extract_begin.set_index(['sha'])
+
+df_extract_end = pd.DataFrame({
+                                'sha': git_ture['merge_commit_sha'],
+                                'time': git_ture['closed_at']
+                            })
+df_extract_end.set_index(['sha'])
+
+df_extract = pd.DataFrame({
+                            'begin_sha': git_ture['base.sha'],
+                            'end_sha': git_ture['merge_commit_sha'],
+                            'commits': git_ture['commits'],
+                            'additions': git_ture['additions'],
+                            'deletions': git_ture['deletions'],
+                            'changed_files': git_ture['changed_files']
+                        })
+
+df_extract.set_index(['begin_sha', 'end_sha'])
+df_extract['total_time'] = pd.to_datetime(df_extract_end['time']) - pd.to_datetime(df_extract_begin['time'])
+time_median = df_extract['total_time'].median()
+
+merged_df_begin = df_extract_begin.merge(sonar_key_set, left_on='sha', right_on='revision')
+merged_df_end = df_extract_end.merge(sonar_key_set, left_on='sha', right_on='revision')
+
+begin = merged_df_begin.merge(sonar_smells, on='key')
+end = merged_df_end.merge(sonar_smells , on='key')
+
+# # Get the column names as a list
+columns_begin = begin.columns.tolist()
+columns_end = end.columns.tolist()
+
+# Add the prefix to each column name using a list comprehension
+columns_with_prefix_begin = ['begin_' + col for col in columns_begin]
+columns_with_prefix_end = ['end_' + col for col in columns_end]
+
+# Assign the modified column names back to the DataFrame
+begin.columns = columns_with_prefix_begin
+end.columns = columns_with_prefix_end
+
+# data Frames Extract GitHub with Begin
+# merged_df = df_extract.merge(begin, on='begin_sha')
+
+first_m = df_extract.merge(begin, on='begin_sha')
+first_m.drop_duplicates()
+second_m = first_m.merge(end , on='end_sha')
+second_m.drop_duplicates()
--- models/01.shiro_extract_column.py
+++ models/01.shiro_extract_column.py
@@ -2,10 +2,7 @@ import pandas as pd
 from datetime import timedelta
 
 git_df = pd.read_csv("../Github/output/03.shiro_label.csv")
-# git_df1 = pd.read_csv("../Github/output/05.shiro.csv")
-# git_df2 = pd.read_csv("../Github/output/03.url_to_pull_353.csv")
-# git_df3 = pd.read_csv("../Github/output/03.pull_to_sub_data_382.csv")
-sonar_df = pd.read_csv("../Sonar/all_projects_data.csv")
+sonar_df = pd.read_csv("../Sonar/output/all_projects_data.csv")
 df_extract = []
 
 df_extract = pd.DataFrame({ 'begin_sha': git_df['base.sha'],

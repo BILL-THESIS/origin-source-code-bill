a307a08b132da0425957292c766930cf034a3e96
beaw21 <beawglom2013@gmail.com>
2023-11-05 22:05:54 +0700

    new kmean set data combination

---
+++ models/KMeans/part1.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from itertools import chain, combinations, permutations
+import numpy as np
+import os
+
+df_original = pd.read_parquet('../../Sonar/seatunnel_all_information.parquet')
+X = df_original.loc[:, ~df_original.columns.isin(['begin_sha', 'end_sha', 'begin_time','end_time',
+                                'total_time',
+                                'time'
+                                ])]
+
+X.iloc[:, [4,5,6,7,8]]
+
+df = pd.DataFrame(X.iloc[:, [4,5,6,7,8]])
+columns = X.columns
+
+directory_path_combia = 'D:\origin-source-code-bill\models\KMeans\combia'
+
+for r in range(1, len(columns) + 1):
+    for column_combination in combinations(columns , r):
+        combined_df = X[list(column_combination)]
+        print(combined_df)
+        combined_df.to_pickle(f"{directory_path_combia}/{combined_df.columns.tolist()}.pkl")

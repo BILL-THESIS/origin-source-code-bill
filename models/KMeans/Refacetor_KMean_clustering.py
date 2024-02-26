05844571c97ff673ad54fb2fca5658df77372e96
patcharaprapakhamkhiaw <bill@patcharaprapakhamkhiaws-MacBook-Air.local>
2024-02-26 11:44:47 +0700

    add platform Linux and Mac

--- models/KMeans/Refacetor_KMean_clustering.py
+++ models/KMeans/Refacetor_KMean_clustering.py
@@ -2,6 +2,8 @@ import multiprocessing
 import random
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
+from multiprocessing.pool import ThreadPool
+import platform
 
 import numpy as np
 import pandas as pd
@@ -16,13 +18,18 @@ from sklearn.preprocessing import MinMaxScaler
 # output: Combination of DataFrame columns
 class KMeansCluster:
 
-    def __init__(self, df_original_20_col: pd.DataFrame, scarler ):
+    def __init__(self, df_original_20_col: pd.DataFrame, scarler, sub_combination):
         self.df = df_original_20_col
         self.scarler = scarler
         self.all_combinations_list_col = [list(itertools.combinations(self.df.columns, r)) for r in
                                           range(2, len(self.df.columns) + 1)]
         self.a = list(itertools.chain(*self.all_combinations_list_col))
         self.all_combinations = [a for a in self.a if len(a) > 0]
+
+        self.sub_combinations = sub_combination
+
+        # self.scarler_fit_transform = self.scarler.fit_transform(self.df)
+        # self.scaled_df = pd.DataFrame(self.scarler_fit_transform, columns=self.df.columns)
 
     @staticmethod
     def chunks(list_combi, n, is_shuffle=False):
@@ -42,6 +49,7 @@ class KMeansCluster:
     def fit_scaler(self, df):
         scaled = self.scarler.fit_transform(df)
         scaled_df = pd.DataFrame(scaled, columns=df.columns)
+        del scaled
         return scaled_df
 
     # step 3
@@ -51,17 +59,38 @@ class KMeansCluster:
     def kmeans_cluster(self, scaled_df_fit_transform):
         results = []
         for n in range(2, 5):
-            kmeans = KMeans(n_clusters=n, n_init=10)
+            kmeans = KMeans(n_clusters=n, n_init=10, max_iter=100, random_state=42)
             kmeans.fit_transform(scaled_df_fit_transform)
             score = silhouette_score(scaled_df_fit_transform, kmeans.labels_)
             results.append((n, score, kmeans.labels_))
         return results
 
+    def loop_cluster(self, sub_list):
+        results = []
+        for i in sub_list:
+            print("I ::", i)
+            for j in i:
+                print("J ::", j)
+                # fit = self.fit_scaler(self.df[list(j)])
+                fit = self.fit_scaler(self.df[list(j)])
+                # print("FIT ::", fit)
+                k = self.kmeans_cluster(fit)
+                k_list = ({
+                    'df': j,
+                    '2': k[0],
+                    '3': k[1],
+                    '4': k[2]})
+                results.append(k_list)
+                print("K ::", k_list)
+                print("Results ::", len(results))
+        return results
 
 
 if __name__ == '__main__':
-    start = time.time()
-    print("start time ::", start)
+    start_time = time.time()
+    start_time_gmt = time.gmtime(start_time)
+    start_time_gmt = time.strftime("%Y-%m-%d %H:%M:%S", start_time_gmt)
+    print(f"start to normalize cluster at: {start_time_gmt}")
 
     minmax_scaler = MinMaxScaler()
     cpus = 4
@@ -71,55 +100,48 @@ if __name__ == '__main__':
     df_original_all_col = pd.read_parquet('../../Sonar/seatunnel_all_information.parquet')
     col_names = df_original_20_col.columns
 
-    bill = KMeansCluster(df_original_20_col, minmax_scaler)
+    all_combinations_list_col = [list(itertools.combinations(df_original_20_col.columns, r)) for r in
+                                 range(2, len(df_original_20_col.columns) + 1)]
 
-    c1 = bill.chunks(bill.all_combinations, 8, False)
+    a1 = list(itertools.chain(*all_combinations_list_col))
+    all_combinations = [a for a in a1 if len(a) > 0]
 
-    sub_c1 = [c[:10] for c in c1]
-    sub_c2 = [c[-10:] for c in c1]
+    check = KMeansCluster.chunks(all_combinations, 8, False)
 
+    sub_c1 = [c[:10] for c in check]
+    sub_c2 = [c[-10:] for c in check]
 
-    def k2(sub_list):
-        results = []
-        for i in sub_list:
-            for j in i:
-                s = bill.fit_scaler(df_original_20_col[list(j)])
-                k = bill.kmeans_cluster(s)
-                k_list = ({
-                    'df': j,
-                    '2': k[0],
-                    '3': k[1],
-                    '4': k[2]})
-                results.append(k_list)
-        return results
+    bill = KMeansCluster(df_original_20_col, minmax_scaler, sub_c1)
 
-    do = k2(sub_c2)
-    do_chunks = bill.chunks(do, 8, False)
-    # do_list = list(do.items() for do in do)
+    # sub = bill.chunks(bill.all_combinations, 8, False)
+    # sub_c2 = [c[-10:] for c in sub]
 
+    k = bill.loop_cluster(sub_c2)
+    k_c = bill.chunks(k, 8, False)
 
-    parsed_description_split = [
-        [list(sub_c2[0]), list(sub_c2[1])],
-        [list(sub_c2[2]), list(sub_c2[3])],
-        [list(sub_c2[4]), list(sub_c2[5])],
-        [list(sub_c2[6]), list(sub_c2[7])]
-    ]
+    if platform.system() == 'Linux':
+        # Code to execute if running on Linux
+        print("Running on Linux")
+        with multiprocessing.pool.Pool(processes=cpus, maxtasksperchild=1) as pool:
+            parsed_split = pool.map(bill.loop_cluster, sub_c2, 3)
+            print("Thread Pool of parsed_data ::", parsed_split)
 
-    # with multiprocessing.pool.ThreadPool(cpus) as pool:
-        # obj is not callable
-        # type object 'KMeansCluster' has no attribute 'k2'
-        # parsed_data = pool.starmap(k2, sub_c2)
-        # parsed_data = pool.starmap(do, parsed_description_split)
-        # print(f"Thread Pool of parsed_data ::", parsed_data)
+    elif platform.system() == 'Darwin':
+        # Code to execute if running on macOS (Darwin is the underlying OS for macOS)
+        print("Running on macOS")
+        with multiprocessing.pool.ThreadPool(cpus) as pool:
+            parsed_description_split = pool.map(bill.kmeans_cluster, list(sub_c2), 3)
+            print("Thread Pool of parsed_data ::", parsed_description_split)
 
-    with ThreadPoolExecutor(max_workers=cpus) as executor:
-        parsed_description_split = list(executor.map(k2, parsed_description_split))
-        print("Thread Pool of parsed_data ::", parsed_description_split)
+    else:
+        # Code to execute if running on another OS
+        print("Running on an unknown or unsupported OS")
 
     end = time.time()
-    total_time = end - start
+    total_time = end - start_time
     time_minutes = total_time / 60
     time_hours = total_time / 3600
 
+    print("total time", total_time)
     print("total_time {:.2f} minutes".format(time_minutes))
     print("Total time {:.2f} hours".format(time_hours))

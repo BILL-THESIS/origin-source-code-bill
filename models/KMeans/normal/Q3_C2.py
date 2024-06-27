319555de80a5e896228872e0d4d07624e8a8d810
patcharaprapakhamkhiaw <bill@patcharaprapakhamkhiaws-MacBook-Air.local>
2024-06-27 14:57:30 +0700

    update cluster of quartile maximum

---
+++ models/KMeans/normal/Q3_C2.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import os
+import joblib
+import time
+import numpy as np
+
+
+def load_data(file_names):
+    for file_name in file_names:
+        with open(os.path.join(file_name), 'rb') as f:
+            results = joblib.load(f)
+    return results
+
+
+def process_cluster(cluster_data):
+    hours = cluster_data['hours']
+    q1 = np.percentile(hours, 25)
+    q3 = np.percentile(hours, 75)
+    median = np.median(hours)
+    equal_q3 = cluster_data[cluster_data['hours'] >= q3]
+    return q1, q3, median, equal_q3
+
+
+def coefficient_of_variation(data):
+    mean_value = np.mean(data)
+    standard_deviation = np.std(data)
+    coefficient_variation = (standard_deviation / mean_value) * 100
+    return coefficient_variation
+
+
+def qurtile_data(cluster_data):
+    cluster_list = []
+    for i in cluster_data:
+        df_col_combined = pd.DataFrame(i)
+        df_col_combined['hours'] = pd.to_timedelta(df_col_combined['total_time']).dt.total_seconds() / 3600
+        name_col = df_col_combined.drop(columns=['num', 'score', 'label', 'total_time', 'hours']).columns.to_list()
+
+        quartile_data = {'col': name_col}
+        # clustr values is cluster number 0, 1, 2
+        for cluster_value in df_col_combined['label'].unique():
+            cluster_data = df_col_combined[df_col_combined['label'] == cluster_value]
+            q1, q3, median, equal_q3 = process_cluster(cluster_data)
+            quartile_data[f'cluter{cluster_value}_q3'] = [q3]
+            quartile_data[f'shape_c{cluster_value}'] = [equal_q3.shape]
+            quartile_data[f'cv_q3_c{cluster_value}'] = [coefficient_of_variation(equal_q3['hours'])]
+            quartile_data[f'label'] = [df_col_combined['label'].tolist()]
+        cluster_list.append(quartile_data)
+
+    return cluster_list
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    start_time_gmt = time.gmtime(start_time)
+    start_time_gmt = time.strftime("%Y-%m-%d %H:%M:%S", start_time_gmt)
+    print(f"start to normalize cluster at: {start_time_gmt}")
+
+    time_str = str(start_time_gmt)
+
+    file_names = (
+        'output/2/results_all2_2024-03-17 15:06:58.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 15:14:06.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 15:25:48.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 15:32:41.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 15:40:12.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 15:50:00.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 16:02:37.parquet.gz',
+        # 'output/2/results_all2_2024-03-17 16:10:28.parquet.gz',
+    )
+
+    results = load_data(file_names)
+    data_quartile = qurtile_data(results)
+    df = pd.DataFrame(data_quartile)
+    print("df q3 :: ", df.columns.to_list())
+
+    df['Q3'] = df.apply(lambda row: (abs(row['cluter0_q3'][0] - row['cluter1_q3'][0]) + abs(
+        row['cluter1_q3'][0] - row['cluter0_q3'][0])) / 3, axis=1)
+
+    df_sort = df.sort_values(by='Q3')[::-1].head(10)
+    print("normal df:: ", df.sort_values(by='Q3').head(10))
+    print("top 10 ::", df_sort)
+
+    df_sort.to_parquet(f'output/q3_c2/q3_c2_{time_str}.parquet')
+
+    # with open(f'output/2/results_all2_{time_str}.parquet.gz', 'wb') as f:
+    #     joblib.dump(results_all2, f, compress=('gzip'))
+    #     print("svae file cluster 2  Done!!")
+
+    end = time.time()
+    total_time = end - start_time
+    time_minutes = total_time / 60
+    time_hours = total_time / 3600
+
+    print("total time", total_time)
+    print("total_time {:.2f} minutes".format(time_minutes))
+    print("Total time {:.2f} hours".format(time_hours))
---
+++ models/KMeans/normal/Q3_C3_top10.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+import joblib
+import time
+import numpy as np
+
+
+def load_data(file_names):
+    df_list = []
+    for file_name in file_names:
+        results = pd.read_parquet(file_name)
+        df_list.append(results)
+        print("Data len all:", len(df_list))
+    return df_list
+
+
+def concat_df_list(lists):
+    concatenated_df = pd.concat(lists)
+    return concatenated_df
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    start_time_gmt = time.gmtime(start_time)
+    start_time_gmt = time.strftime("%Y-%m-%d %H:%M:%S", start_time_gmt)
+    print(f"start to normalize cluster at: {start_time_gmt}")
+
+    time_str = str(start_time_gmt)
+
+    file_names = (
+        '../output/q3_c3/q3_c3_2024-03-28 06:00:11.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 06:14:04.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 06:28:03.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 06:42:11.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 06:55:51.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 07:09:59.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 07:24:15.parquet',
+        '../output/q3_c3/q3_c3_2024-03-28 07:37:25.parquet',
+
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 08:59:56.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 09:15:24.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 09:34:58.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 09:51:22.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 10:06:08.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 10:20:41.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 10:36:13.parquet',
+        # 'output/q3_c3/q3_c3_otliers2024-04-28 10:50:39.parquet',
+    )
+
+    data = load_data(file_names)
+    df = concat_df_list(data)
+    df.drop_duplicates(inplace=True)
+
+    # df_sort_max = df.sort_values(by='distance_q3', ascending=False).head(10)
+    # df_sort_min = df.sort_values(by='distance_q1', ascending=True).head(10)
+
+    df_sort_max = df.sort_values(by='Q3', ascending=False).head(10)
+    df_sort_min = df.sort_values(by='Q1', ascending=True).head(10)
+
+    # print("DF", df_sort_max['Q3'])
+    # print("========")
+    # print("DF Min ::", df_sort_min['Q1'])
+
+    # df_sort_max.to_parquet(f'../output/q3_c3/q3_c3_top_10_outliers{time_str}.parquet')
+    # df.to_parquet(f'../output/q3_c3/q3_c3_not_sort_{time_str}.parquet')
+
+
+    end = time.time()
+    total_time = end - start_time
+    time_minutes = total_time / 60
+    time_hours = total_time / 3600
+
+    print("total time", total_time)
+    print("total_time {:.2f} minutes".format(time_minutes))
+    print("Total time {:.2f} hours".format(time_hours))

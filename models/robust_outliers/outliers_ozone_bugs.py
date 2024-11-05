ba03135237589292a81e09408c93e7e6ef1032b3
beaw21 <beawglom_2013@gmail.com>
2024-11-05 16:14:37 +0700

    fix the value total time to date time

--- models/robust_outliers/outliers_ozone_bugs.py
+++ models/robust_outliers/outliers_ozone_bugs.py
@@ -3,34 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def calculate_smell_bug(df: pd.DataFrame) -> pd.DataFrame:
-    # rename_dict = {
-    #     'Dispensables_created': 'created_d',
-    #     'Bloaters_created': 'created_b',
-    #     'Change Preventers_created': 'created_cp',
-    #     'Couplers_created': 'created_c',
-    #     'Object-Orientation Abusers_created': 'created_ooa',
-    #     'Uncategorized_created': 'created_u',
-    #     'Dispensables_ended': 'ended_d',
-    #     'Bloaters_ended': 'ended_b',
-    #     'Change Preventers_ended': 'ended_cp',
-    #     'Couplers_ended': 'ended_c',
-    #     'Object-Orientation Abusers_ended': 'ended_ooa',
-    #     'Uncategorized_ended': 'ended_u'
-    # }
-    # df = df.rename(columns=rename_dict)
+def calculate_total_time(df: pd.DataFrame) -> pd.DataFrame:
     df['total_time'] = pd.to_datetime(df['merged_at']) - pd.to_datetime(df['created_at'])
-    # df['value_ended'] = pd.to_numeric(df['value_ended'], errors='coerce')
-    # df['value_created'] = pd.to_numeric(df['value_created'], errors='coerce')
-    # df['diff_bug'] = df['value_ended'] - df['value_created']
-    # df['percentage_bug'] = ((df['value_ended'] - df['value_created']) / df['value_created']) * 100
 
-    # for col in ['d', 'b', 'cp', 'c', 'ooa', 'u']:
-    #     df[f'diff_{col}'] = df[f'ended_{col}'] - df[f'created_{col}']
-    #     df[f'percentage_{col}'] = ((df[f'ended_{col}'] - df[f'created_{col}']) / df[f'created_{col}']) * 100
+    # Calculate years and months from total_time
+    df['completed_date'] = pd.to_datetime(df['created_at']) + df['total_time']
 
     # Add a new column for year-month
-    df['year_month'] = pd.to_datetime(df['created_at']).dt.to_period('M')
+    df['year_month'] = pd.to_datetime(df['completed_date']).dt.to_period('M')
 
     return df
 
@@ -42,17 +22,30 @@ def robust_outlier_detection(df: pd.DataFrame, threshold: float = 2.24) -> tuple
     mad = np.abs(data - median).median()
     madn = mad / 0.6745
 
+    # Calculate lower and upper bounds for outliers
     low_outliers = (data - median) / madn < -threshold
     high_outliers = (data - median) / madn > threshold
+    print("Lower  outlier: ", low_outliers)
+    print("Upper outlier: ", high_outliers)
+    print("\n")
+
+    # Identify outliers based on the bounds
+    outlier = (data - median).abs() / madn > threshold
+    print("Sum outliers :", outlier.sum())
 
     df_low_outliers = df[low_outliers]
     df_high_outliers = df[high_outliers]
     df_normal = df[~(low_outliers | high_outliers)]
 
+    print(
+        f'Low outliers: {df_low_outliers.shape[0]} ,'
+        f'High outliers: {df_high_outliers.shape[0]} '
+        f'Normal: {df_normal.shape[0]}')
+
     return df_normal, df_low_outliers, df_high_outliers
 
 
-def plot_compare_shape(summary_df: pd.DataFrame, project_name: str):
+def plot_compare_outlier(summary_df: pd.DataFrame, project_name: str):
     bar_width = 0.25
     plt.figure(figsize=(15, 8))
 
@@ -71,16 +64,17 @@ def plot_compare_shape(summary_df: pd.DataFrame, project_name: str):
     plt.legend()
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
-    plt.savefig(f'../../models/robust_outliers/output/{project_name}_outliers.png')
+    plt.savefig(f'../../models/robust_outliers/output/{project_name}_robits_outliers.png')
     plt.show()
 
 
 if __name__ == '__main__':
     data_original = pd.read_pickle("../../Github/output/ozone_filtered_issue_bug.pkl")
-    data_original = calculate_smell_bug(data_original)
+    data_original = calculate_total_time(data_original)
 
     # Group by `year_month` instead of `year`
     grouped = data_original.groupby('year_month')
+    normal, low_outliers, high_outliers = robust_outlier_detection(data_original)
     results = {period: robust_outlier_detection(group) for period, group in grouped}
 
     summary_df = pd.DataFrame([
@@ -94,4 +88,4 @@ if __name__ == '__main__':
     ])
 
     summary_df.reset_index(drop=True, inplace=True)
-    plot_compare_shape(summary_df, 'ozone')
+    plot_compare_outlier(summary_df, 'ozone-total-time')

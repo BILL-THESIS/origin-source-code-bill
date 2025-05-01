3c835e2421921640e309ed92ea6722cc3e9b654b
beaw21 <beawglom_2013@gmail.com>
2025-05-01 12:15:12 +0700

    resample data x and y

---
+++ dynamic/02.resample_data/read_file_x_y.py
@@ -0,0 +1,6 @@
+import joblib
+import pandas as pd
+
+seat = joblib.load("../output/seatunnel_resampled_data_2may.pkl")
+ozone = joblib.load("../output/ozone_resampled_data_2may.pkl")
+pulsar = joblib.load("../output/ozone_resampled_data_2may.pkl")

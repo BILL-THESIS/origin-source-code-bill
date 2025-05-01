7b30035ea4ae75ec282c16ec0647585e672ef878
beaw21 <beawglom_2013@gmail.com>
2025-05-01 12:13:48 +0700

    Check the status of man whithey

---
+++ dynamic/Rechack_pvalue.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+def get_status(df):
+    df['status'] = pd.cut(
+        df['d_value'],
+        bins=[-float('inf'), 0.14, 0.33, 0.474, float('inf')],
+        labels=["negligible", "small", "medium", "large"]
+    )
+    return df
+
+
+if __name__ == "__main__":
+    ozone = pd.read_pickle("/Users/bill/origin-source-code-bill/dynamic/output/output/ozone_all_status_significant.pkl")
+    seatunnel = pd.read_pickle(
+        "/Users/bill/origin-source-code-bill/dynamic/output/output/seatunnel_all_status_significant.pkl")
+    pulsar = pd.read_pickle(
+        "/Users/bill/origin-source-code-bill/dynamic/output/output/pulsar_all_status_significant.pkl")
+
+    sea_man = seatunnel[seatunnel["p_value"] < 0.05]
+    pulsar_man = pulsar[pulsar["p_value"] < 0.05]
+    ozone_man = ozone[ozone["p_value"] < 0.05]
+
+    sea_man = get_status(sea_man)
+    pulsar_man = get_status(pulsar_man)
+    ozone_man = get_status(ozone_man)
+
+    # count status
+    status_counts = sea_man["status"].value_counts()
+    print(status_counts)
+    pulsar_counts = pulsar_man["status"].value_counts()
+    print(pulsar_counts)
+    ozone_counts =ozone_man["status"].value_counts()
+    print(ozone_counts)
+
+    # result count
+    df = pd.concat([ozone_counts, pulsar_counts, status_counts], axis=1)
+    df.columns = ['ozone', 'pulsar', 'seatunnel']
+    df.sort_values("status", ascending=False, inplace=True)
+
+    sea_man_final = sea_man[sea_man["status"].isin(["large", "medium"])]
+    pulsar_man_final = pulsar_man[pulsar_man["status"].isin(["large", "medium"])]
+    ozone_man_final = ozone_man[ozone_man["status"].isin(["large", "medium"])]

b73451c8f9cf8d95daaa43967812c5d1ddf468c8
beaw21 <beawglom_2013@gmail.com>
2025-06-06 14:39:31 +0700

    update code resample data

--- dynamic/02.resample_data/read_file_x_y.py
+++ dynamic/02.resample_data/read_file_x_y.py
@@ -4,3 +4,7 @@ import pandas as pd
 seat = joblib.load("output_resample/seatunnal_resampled.pkl")
 ozone = joblib.load("output_resample/ozone_resampled_data.pkl")
 pulsar = joblib.load("output_resample/pulsar_resampled_data.pkl")
+
+o_new = joblib.load("output_resample/ozone_resampled_combinations_new.pkl")
+p_new = joblib.load("output_resample/pulsar_resampled_combinations_new.pkl")
+s_new = joblib.load("output_resample/seatunnal_resampled_combinations_new.pkl")
